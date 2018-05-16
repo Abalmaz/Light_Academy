230b88609f02b7b0df5febc1fc835fac08703030
abalmazik <yana.abalmaz@gmail.com>
2018-05-16 19:39:44 +0300

    Add base method for class Unit

--- Battler/unit.py
+++ Battler/unit.py
@@ -1,21 +1,55 @@
 from abc import ABCMeta
+'''
+класс для всех, счет времени для восстанавления
+'''
+class Clock:
+    def __init__(self):
+        self.i = i
+    def tick(self):
+        self.i += 1
+    def time(self):
+        return self.i    
 
 class Unit(metaclass = ABCMeta):
 	
-	@abstractmethod
-	def attack(self, target):
-		pass
+    '''
+      при аттаке приплюсовываем время для воостановления
+      target.take_demage()
+      self._recharge_to = self.clock.time()+1000
+      активны все у кого recharge <= текущего времени   
+    '''
+    @abstractmethod
+    def attack(self, target):
+        pass
     
+    @abstractmethod
     def take_demage(self, dmg):
-    	pass
+        pass
 
     @property
     @abstractmethod
     def alive(self):
         pass
 
-    def healf(self):
+    @property
+    @abstractmethod
+    def health(self):
         pass
 
+    @property
+    @abstractmethod
     def attack_power(self):
-        pass        	
+        pass
+
+    '''
+    когда recharge == 0 можно аттаковать
+    '''
+    @property
+    @abstractmethod
+    def recharge(self):
+        pass
+
+    '''
+    
+    '''
+                	
