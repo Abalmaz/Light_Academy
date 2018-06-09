d8b1c7f22dcc16ff1842a4450031381d6afa7da6
abalmazik <yana.abalmaz@gmail.com>
2018-06-09 13:26:28 +0300

    Add django

---
+++ Django/myproj/myapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    text = models.CharField(max_length=64)
+    checked = models.BooleanField()
+
+    # class Meta:
+    #     verbose_name = 'task model'
+    #     verbose_name_plural = 'task model'
+    #     unique_together=['text', 'checked']
---
+++ Django/myproj/myapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from myapp.models import Task
+
+
+# Create your views here.
+def index(request):
+    tasks = Task.objects.all()
+    return render(request, 'myapp/index.html', {'tasks': tasks})
+
+
+def create_task(request):
+
+    if request.method == 'POST':
+        task = Task(text=request.POST.get('text'), checked=bool(request.POST.get('checked', False)))
+        task.save()
+
+    return redirect('/tasks')
+
+def detail(request, pk):
+    try:
+        task = Task.objects.get(id=pk)
+    except Task.DoesNotExist:
+        print('Exception')
+
+    return render(request, 'myapp/detail.html', {'task': task})
+
